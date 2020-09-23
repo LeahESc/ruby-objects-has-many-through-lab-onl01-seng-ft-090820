e146618844bf6836e52b278d38ca4ce0c7eeb15c
Leah Schlackman <leahemily1@gmail.com>
2020-09-23 23:53:23 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -11,3 +11,13 @@ class Genre
   def self.all 
     @@all 
   end 
+  
+  def songs 
+    Song.all.select {|song| song.genre == self}
+  end 
+  
+  def artists 
+    self.songs.map do |song|
+      song.artist 
+    end
+  end 
