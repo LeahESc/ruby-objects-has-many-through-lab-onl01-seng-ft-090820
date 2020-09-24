20c0799460fd6233943ea7046d925f67a536ba5c
Leah Schlackman <leahemily1@gmail.com>
2020-09-24 00:10:54 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -2,3 +2,15 @@ class Appointment
   
   @@all = [] 
   
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  
+  def self.all
+    @@all
+  end 
+  
+  
+end 
